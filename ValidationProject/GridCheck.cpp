48142a4b5eb74bee81d418d64112cd1415ce22f0
inventor56 <joshner.smith@gmail.com>
2017-09-24 15:47:53 -0700

    - Initial Commit, moved project to Git

---
+++ ValidationProject/GridCheck.cpp
@@ -0,0 +1,5 @@
+//
+// Created by joshsmith on 9/24/17.
+//
+
+#include "GridCheck.h"
---
+++ ValidationProject/GridCheck.h
@@ -0,0 +1,14 @@
+//
+// Created by joshsmith on 9/24/17.
+//
+
+#ifndef UNTITLED_GRIDCHECK_H
+#define UNTITLED_GRIDCHECK_H
+
+
+class GridCheck {
+
+};
+
+
+#endif //UNTITLED_GRIDCHECK_H
