efadb4ab972a96f2bab9fa473814ff8dac6c0be8
inventor56 <joshner.smith@gmail.com>
2017-10-06 22:55:49 -0700

    - Some more testing done, still haven't discovered error quite yet

--- ValidationProject/AnswerClass.cpp
+++ ValidationProject/AnswerClass.cpp
@@ -4,17 +4,26 @@
 
 #include "AnswerClass.h"
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 list<ErrorObject> AnswerClass::findAnswer(int **arr, list<ErrorObject> errorList) {
     // Find the answers.
     // Check each object against the others.
+
+    if (errorList.size() == 1) { // Only one object
+        errorList.front().setAnswer(checkRowOrColumn(0, arr, errorList.front()));
+        return errorList;
+
+    }
+
     for (auto &objectToLookAt : errorList) {
         bool avoidRow = false;
         bool avoidColumn = false;
 
         // If the object shares the same row or column, take a note of that.
+
         for (auto &objectsToCheckAgainst : errorList) {
             if (&objectToLookAt !=
                 &objectsToCheckAgainst) { // May be an issue here! You need to be sure you are comparing the objects correctly
@@ -27,16 +36,20 @@ list<ErrorObject> AnswerClass::findAnswer(int **arr, list<ErrorObject> errorList
             }
         }
 
+
         // Then hop into whichever check doesn't share a spot with the others
         // Check is complete, see if you have the opportunity to check another row or column for the correct solution
         // Check either the row, column, or grid, depending on where the other objects lie, set the error object
-        if (!avoidRow) {
+        if (!avoidRow && avoidColumn) {
             objectToLookAt.setAnswer(checkRowOrColumn(0, arr, objectToLookAt));
         }
-        if (!avoidColumn) { // This may be double checking :-/
+        else if (!avoidColumn && avoidRow) {
             objectToLookAt.setAnswer(checkRowOrColumn(1, arr, objectToLookAt));
         }
-        if (avoidColumn || avoidRow) {
+        else if (!avoidColumn && !avoidRow) {
+            objectToLookAt.setAnswer(checkRowOrColumn(0, arr, objectToLookAt));
+        }
+        else {
             // Set answer to impossible to difficult to solve in current grid state
             objectToLookAt.setAnswer(-1); // Make sure -1 will work
         }
@@ -56,8 +69,8 @@ list<ErrorObject> AnswerClass::findAnswer(int **arr, list<ErrorObject> errorList
 
 int AnswerClass::checkRowOrColumn(int version, int** array, ErrorObject errorObj) {
 
-    vector<int> areaVector; // Set this to be either the row or column vector
-
+    vector<int> areaVector (9); // Set this to be either the row or column vector
+    cout << "Here is the answer in object" << errorObj.getAnswer() << endl;
     for (int i = 0; i < 9; i++) {
         if(version == 0) {
             areaVector[i] = array[errorObj.getRow()][i]; // Same row, but the column is different (so we check horizontally)
@@ -80,6 +93,7 @@ int AnswerClass::checkRowOrColumn(int version, int** array, ErrorObject errorObj
     for (int i = 1; i <= areaVector.size(); i++){
         if (areaVector.at(static_cast<unsigned long>(i)-1) != i) {
             // This is the correct answer, because we could not find it in the sorted list, it's the missing link
+            cout << "Here is the answer in object" << errorObj.getAnswer() << endl;
             return i;
         }
     }
